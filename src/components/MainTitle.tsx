4712e58a958ecaee4bcc1aa93db160a3e0a93d30
andreabr4 <br13.andrea@gmail.com>
2024-01-29 17:20:40 +0100

    introduce motion in main title

--- src/components/MainTitle.tsx
+++ src/components/MainTitle.tsx
@@ -1,16 +1,59 @@
 import mug from "../../images/Mug1.png";
+import { motion } from "framer-motion";
+
 export default function MainTitle() {
   return (
     <>
-      <header className="container-fluid mb-5 mt-5 ps-5 pe-5" >
-            <h1 className="title-words title_container" >
-              <span className="first-line-title">LOREM</span>
+      <header className="container-fluid mb-5 mt-5 ps-5 pe-5">
+        <h1 className="title-words title_container">
+          <motion.span
+            initial={{ x: -1500 }}
+            animate={{ x: 0 }}
+            transition={{
+              duration: 2,
+              delay: 0.1,
+            }}
+            // whileHover={{scale: 0.9}}
+            className="first-line-title"
+          >
+            LOREM
+          </motion.span>
 
-              <span className="second-line-title" >IPSUM</span>
+          <motion.span
+            initial={{ x: 1500 }}
+            animate={{ x: 0 }}
+            transition={{
+              duration: 2,
+              delay: 0.2,
+            }}
+            // whileHover={{ opacity: 0.8 }}
+            className="second-line-title"
+          >
+            IPSUM
+          </motion.span>
 
-              <span className="third-line-title" >DOLOR</span>
-              <img className="title-mug" src={mug}  ></img>
-            </h1>
+          <motion.span
+            initial={{ x: -1500 }}
+            animate={{ x: 0 }}
+            transition={{
+              duration: 2,
+              delay: 0.5,
+            }}
+            // whileHover={{ opacity: 0.8 }}
+            className="third-line-title"
+          >
+            DOLOR
+          </motion.span>
+
+          <motion.img 
+             initial={{ y: -1500 }}
+             animate={{ y: 0 }}
+             transition={{
+               duration: 2,
+             }}
+
+          className="title-mug" src={mug}></motion.img>
+        </h1>
       </header>
     </>
   );
