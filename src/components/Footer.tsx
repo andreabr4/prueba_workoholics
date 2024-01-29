9d617b825a17ac769a97d7bf92c6e8f2187d3adf
andreabr4 <br13.andrea@gmail.com>
2024-01-29 20:30:56 +0100

    first attempt footer mobile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,9 @@
 export default function Footer() {
   return (
     <>
-      <div className="container-fluid footer-container p-5" id="page-footer" >
+      <div className="container-fluid footer-container p-5" id="page-footer">
         <div className="row mt-4">
-          <div className="col-5 contact-information">
-            <div className="row">
-            <a href="mailto:hello@workoholics.es">hello@workoholics.es</a>
-            </div>
-            <div className="row">
-            <a href="tel:+34 944 059 957">+34 944 059 957</a>
-            </div>
-          </div>
-
-          <div className="col-7">
+          <div className="col-md-7 order-md-2">
             <div className="row-6 footer-slogan">
               <p>We love what we do.</p>
               <p>We are Worköholics.</p>
@@ -21,48 +12,64 @@ export default function Footer() {
             <form>
               <div className="row mb-3">
                 <div className="col">
-                <input
-                  className="footer-form form-control"
-                  type="text"
-                  name="email"
-                  placeholder="E-mail"
-                />
-
-                <div className="row mt-4">
-                  <div className="col-auto form-check">
                   <input
-                    type="checkbox"
-                    className="form-check-privacy"
-                    name="privacy"
-                    id="privacy"
+                    className="footer-form form-control"
+                    type="text"
+                    name="email"
+                    placeholder="E-mail"
                   />
-                  <label className="form-check-label ms-3" htmlFor="privacy">
-                    {"I have read and accepted the "}
-                    <a href="" className="privacy-link">Terms and Conditions.</a>
-                  </label>
+
+                  <div className="row mt-4">
+                    <div className="col-auto form-check">
+                      <input
+                        type="checkbox"
+                        className="form-check-privacy"
+                        name="privacy"
+                        id="privacy"
+                      />
+                      <label
+                        className="form-check-label ms-3"
+                        htmlFor="privacy"
+                      >
+                        {"I have read and accepted the "}
+                        <a href="" className="privacy-link">
+                          Terms and Conditions.
+                        </a>
+                      </label>
+                    </div>
+                    <div className="col-auto">
+                      <button type="submit" className="footer-submit-btn">
+                        Submit
+                        <svg
+                          fill="none"
+                          viewBox="0 0 34 25"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M1 0v15.5h29.808"
+                            className="white-stroke"
+                            stroke-width="2"
+                          ></path>
+                          <path
+                            d="M23.654 7.154L32 15.5l-8.346 8.346"
+                            className="white-stroke"
+                            stroke-width="2"
+                          ></path>
+                        </svg>
+                      </button>
+                    </div>
+
+                    <div className="col-md-5 order-md-1 mt-sm-5 contact-information">
+                      <div className="row">
+                        <a href="mailto:hello@workoholics.es">
+                          hello@workoholics.es
+                        </a>
+                      </div>
+                      <div className="row">
+                        <a href="tel:+34 944 059 957">+34 944 059 957</a>
+                      </div>
+                    </div>
                   </div>
-                  <div className="col-auto ms-5">
-                  <button type="submit" className="footer-submit-btn">
-                    Submit
-                    <svg
-                    fill="none"
-                    viewBox="0 0 34 25"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M1 0v15.5h29.808"
-                      className="white-stroke"
-                      stroke-width="2"
-                    ></path>
-                    <path
-                      d="M23.654 7.154L32 15.5l-8.346 8.346"
-                      className="white-stroke"
-                      stroke-width="2"
-                    ></path>
-                  </svg>
-                  </button>
-                  </div>
-                </div>
                 </div>
               </div>
             </form>
